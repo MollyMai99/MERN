b88fac7e36da6dfd3b4e78dfd07479d7559d89be
MollyMai99 <miaoling.mai99@gmail.com>
2024-05-09 11:33:56 +0800

    feat: create draft pages, conditional render AuthPage, setup routes"

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,12 +1,34 @@
 import debug from "debug";
+import { useState } from "react";
+import AuthPage from "../AuthPage/AuthPage";
+import NewOrderPage from "../NewOrderPage/NewOrderPage";
+import OrderHistoryPage from "../OrderHistoryPage/OrderHistoryPage";
+import { Route, Routes } from "react-router-dom";
 
 const log = debug("mern:pages:App:App");
 
 function App() {
-  log("inside App");
+  // eslint-disable-next-line no-unused-vars
+  const [user, setUser] = useState("a");
+  log("user %o", user);
+
+  if (!user) {
+    return (
+      <main className="App">
+        <AuthPage />
+      </main>
+    );
+  }
+
   return (
     <>
-      <main className="App">App</main>
+      <main className="App">
+        <Routes>
+          <Route path="/orders" element={<OrderHistoryPage />} />
+          <Route path="/orders/:id" element={<OrderHistoryPage />} />
+          <Route path="/orders/new" element={<NewOrderPage />} />
+        </Routes>
+      </main>
     </>
   );
 }
